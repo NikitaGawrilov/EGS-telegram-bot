1a39e2b432c8a78411c36b0cdb8fe90cbbb7438e
NikitaGawrilov <gawr.nik@gmail.com>
2022-12-11 23:22:09 +0500

    added locale options & exception handlers

--- games_getter.py
+++ games_getter.py
@@ -1,14 +1,41 @@
 import requests
 import json
+from json import JSONDecodeError
 
+locales = {
+    "ru": ("ru-RU", "RU"),
+    "us": ("en-US", "US")
+}
 
-def get_free_games() -> dict:
-    api_url = (
-        'https://store-site-backend-static.ak.epicgames.com/'
-        'freeGamesPromotions?locale=ru-RU&country=RU'
-    )
-    data = requests.get(api_url).json()
-    return data
+def get_free_games(locale: str = "ru"):
+    try:
+        api_url = (
+            f'https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?'
+            f'locale={locales[locale][0]}&'
+            f'country={locales[locale][1]}'
+        )
+    except KeyError:
+        raise Exception("Incorrect locale given")
+
+    try:
+        raw_data = requests.get(api_url).json()
+    except JSONDecodeError:
+        raise Exception("Seems like there are some problems with getting or decoding response from EGS servers")
+
+    all_games = raw_data.get("data").get("Catalog").get("searchStore").get("elements")
+    if not all_games:
+        raise Exception("No games returned by EGS server")
+
+    currently_free = [game for game in all_games
+                      if game.get("promotions") and game.get("promotions").get("promotionalOffers")]
+
+    upcoming_free = [game for game in all_games
+                     if game.get("promotions") and game.get("promotions").get("upcomingPromotionalOffers")]
+
+    return {
+        "cur": currently_free,
+        "upcome": upcoming_free
+    }
 
 
 free = get_free_games()

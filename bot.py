797cde73921f3de2ad8592681a4d4b454040adc5
NikitaGawrilov <gawr.nik@gmail.com>
2022-12-12 18:35:46 +0500

    base dialog menu added

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
-import asyncio
 from aiogram import Bot, Dispatcher, executor, types
-from loguru import logger
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from config import BOT_TOKEN
 
 bot = Bot(token=BOT_TOKEN)
@@ -8,7 +7,25 @@ disp = Dispatcher(bot)
 
 @disp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await message.answer("Hi there!")
+    base_kb = ReplyKeyboardMarkup(
+        [[KeyboardButton("Что раздают сейчас?"), KeyboardButton("Что раздадут следующим?")]],
+        resize_keyboard=True,
+        one_time_keyboard=False
+    )
+    await message.answer("Hi there!", reply_markup=base_kb)
+
+
+@disp.message_handler(lambda message: message.text == "Что раздают сейчас?")
+async def current_giveaway(message: types.Message):
+    await message.reply(f"Вот что раздают бесплатно в EGS в данный момент:\n"
+                        f"-///-")\
+
+
+@disp.message_handler(lambda message: message.text == "Что раздадут следующим?")
+async def current_giveaway(message: types.Message):
+    await message.reply(f"Вот что раздадут бесплатно в EGS в ближайшее время:\n"
+                        f"-///-")
+
 
 if __name__ == "__main__":
     executor.start_polling(disp, skip_updates=True)
